e735b9baab13908bfe6cfeb98d34776ea0c75258
Kunal Pandey <10c.kunalpandey@gmail.com>
2026-05-31 00:45:04 +0530

    Immplemented: Binding of socket to INADRR_ANY addr. and PORT 8000

--- webserver.c
+++ webserver.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <winsock2.h>
 
+#define PORT 8000
+
 int main(){
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) !=0 ){
@@ -15,5 +17,19 @@ int main(){
     
     printf("Socket successfully created.\n");
 
-    closesocket(socketfd);
+    struct sockaddr_in host_addr;
+    int host_addr_len = sizeof(host_addr);
+
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(socketfd,(struct sockaddr*)&host_addr,host_addr_len)!=0){
+        printf("Socket Binding Error : %s\n",WSAGetLastError());
+        return 1;
+    }
+
+    printf("Binding of the socket done successfully.");
+
+    // closesocket(socketfd);
 }
