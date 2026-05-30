7407a1b1dabe6fb5df0173019d7681fe2a5cf704
Kunal Pandey <10c.kunalpandey@gmail.com>
2026-05-30 23:17:26 +0530

    Implement: TCP Socket using winsock header

--- webserver.c
+++ webserver.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <winsock2.h>
 
 int main(){
-    printf("Sup Mfs");
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) !=0 ){
+        printf("Failed to inialize the Winsock\n");
+    }
+
+    SOCKET socketfd = socket(AF_INET,SOCK_STREAM,0);
+
+    if(socketfd==-1){
+        printf("Socket failed : %d\n",WSAGetLastError());
+    }
+    
+    printf("Socket successfully created.\n");
+
+    closesocket(socketfd);
 }
